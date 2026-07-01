64f50400a311ed1f9bc89092a12adb7ac8b2ff2f
Johnathon47 <hei.johnathon.2@gmail.com>
2026-07-01 18:00:12 +0300

    chore: add file that do the algorithm of guess number

---
+++ devineNombre.py
@@ -0,0 +1,31 @@
+import random
+
+name = input("Enter your name: ")
+print("----------------------------------------")
+print(f"       Hello {name} 👋!")
+print("----------------------------------------")
+print("| Welcome in the game 'Guess number' ! |")
+print("----------------------------------------")
+
+print("I choice number between 1 and 100. try to guess him.")
+
+secret_number = random.randint(1, 100)
+tries = 0
+guess = False
+
+while not guess:
+    proposition = input("Your proposition: ")
+    if not proposition.isdigit():
+        print("Enter a number valid.")
+        continue
+    
+    proposition = int(proposition)
+    tries += 1
+    
+    if proposition < secret_number:
+        print(f"Oops! That's number {proposition} is so small 🤏!")
+    elif proposition > secret_number:
+        print(f"Oops! That's number {proposition} is so big 🥺!")
+    else:
+        print(f"Great {name} 🥳🪇! You guessed in {tries} tries.")
+        guess = True

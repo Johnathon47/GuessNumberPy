d4f8fcd36dba625bdbe9802ef76a33a58d92e859
Johnathon47 <hei.johnathon.2@gmail.com>
2026-07-01 19:56:25 +0300

    chore: structure the code in function

--- devineNombre.py
+++ devineNombre.py
@@ -1,31 +1,44 @@
 import random
 
 name = input("Enter your name: ")
-print("----------------------------------------")
-print(f"       Hello {name} 👋!")
-print("----------------------------------------")
-print("| Welcome in the game 'Guess number' ! |")
-print("----------------------------------------")
 
-print("I choice number between 1 and 100. try to guess him.")
 
+def welcome():
+    print("----------------------------------------")
+    print(f"       Hello {name} 👋!")
+    print("----------------------------------------")
+    print("| Welcome in the game 'Guess number' ! |")
+    print("----------------------------------------")
+    print("I choice number between 1 and 100. try to guess him.")
+
+
+# Variables
 secret_number = random.randint(1, 100)
 tries = 0
 guess = False
 
-while not guess:
-    proposition = input("Your proposition: ")
-    if not proposition.isdigit():
-        print("Enter a number valid.")
-        continue
-    
-    proposition = int(proposition)
-    tries += 1
-    
-    if proposition < secret_number:
-        print(f"Oops! That's number {proposition} is so small 🤏!")
-    elif proposition > secret_number:
-        print(f"Oops! That's number {proposition} is so big 🥺!")
-    else:
-        print(f"Great {name} 🥳🪇! You guessed in {tries} tries.")
-        guess = True
+
+# Guess algorithm
+def guessGame(secret_number, tries, guess):
+    welcome()
+    while not guess:
+        proposition = input("Your proposition: ")
+        if not proposition.isdigit():
+            print("Enter a number valid.")
+            continue
+
+        proposition = int(proposition)
+        tries += 1
+
+        if proposition < secret_number:
+            print(f"Oops! That's number {proposition} is so small 🤏!")
+        elif proposition > secret_number:
+            print(f"Oops! That's number {proposition} is so big 🥺!")
+        else:
+            print(f"Great {name} 🥳🪇! You guessed in {tries} tries.")
+            guess = True
+
+
+# Run the Game
+guessGame(secret_number, tries, guess)
+
